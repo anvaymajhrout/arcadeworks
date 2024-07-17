3064672084f13ba374748ecc75c6e39e23fbb412
anvaymajhrout <amajhrout@gmail.com>
2024-07-17 17:24:05 +0530

    New Year Counter

---
+++ newyearcountdown/script.js
@@ -0,0 +1,18 @@
+function countdown() {
+    const now = new Date().getTime();
+    const nextYear = new Date(`January 1, ${new Date().getFullYear() + 1} 00:00:00`).getTime();
+    const timeRemaining = nextYear - now;
+
+    const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+    document.getElementById('days').innerText = days;
+    document.getElementById('hours').innerText = hours;
+    document.getElementById('minutes').innerText = minutes;
+    document.getElementById('seconds').innerText = seconds;
+}
+
+setInterval(countdown, 1000);
+countdown();  
